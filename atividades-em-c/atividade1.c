59358a81d655fe2d6bc0afc9745845068f1d1b0e
Gabriel Rodrigues <gabrielrodrigues.dev2@gmail.com>
2026-05-18 20:44:48 -0300

    Update atividade1.c

--- atividades-em-c/atividade1.c
+++ atividades-em-c/atividade1.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main()
+{
+        float peso;
+        float altura;
+        
+        printf("Digite seu peso: ");
+        scanf("%f", &peso);
+        printf("Digite sua altura: ");
+        scanf("%f", &altura);
+        float imc = peso / (altura * altura);
+        printf("IMC: %.2f\n", imc);
+        if(imc < 18.5){
+            printf("Abaixo do Peso");
+        }else if(imc >= 18.5 && imc < 24.9){
+            printf("Normal");
+        }else if(imc >= 25 && imc < 29.9){
+            printf("Sobrepeso");
+        }else{
+            printf("Obeso");
+        }
+        
+
+    return 0;
+}
